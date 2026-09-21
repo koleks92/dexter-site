142a3ee5dc66c6d159f83420f56f977edebeb154
koleks92_NEXT <koleks92@gmail.com>
2026-09-21 13:54:29 +0200

    feat: lightshow

---
+++ scripts/gallery.js
@@ -0,0 +1,21 @@
+const thumbnails = document.querySelectorAll(".thumbnail");
+const modal = document.querySelector("#modal");
+const closeButton = modal.querySelector("div");
+
+// Close modal
+closeButton.addEventListener("click", () => {
+  modal.classList.toggle("active");
+});
+
+// Open modal and change the image
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", (event) => {
+    // Open modal
+    modal.classList.toggle("active");
+    // Change img and figcation
+    modal
+      .querySelector("img")
+      .setAttribute("src", thumbnail.getAttribute("src").replace("_600", ""));
+    modal.querySelector("figcaption").innerHTML = thumbnail.getAttribute("alt");
+  });
+});
