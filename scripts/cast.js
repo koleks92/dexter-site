ad23b918b51b1a4287691d7e706e043d4816f51e
koleks92_NEXT <koleks92@gmail.com>
2026-09-21 10:35:37 +0200

    feat: Added Read More/Read less

---
+++ scripts/cast.js
@@ -0,0 +1,11 @@
+const readMoreButtons = document.querySelectorAll(".read_more");
+
+// Event listener
+readMoreButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // Switch visibility
+    button.previousElementSibling.classList.toggle("active");
+    button.textContent =
+      button.textContent === "Read more..." ? "Read less" : "Read more...";
+  });
+});
